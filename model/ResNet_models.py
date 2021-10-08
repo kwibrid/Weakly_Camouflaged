0fa635338707574067b0d7a49a43daa0c65678ef
kwibrid <34770671+kwibrid@users.noreply.github.com>
2021-10-08 18:23:06 +1100

    Add files via upload

---
+++ model/ResNet_models.py
@@ -0,0 +1,283 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from model.ResNet import B2_ResNet
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from torch.nn import Parameter, Softmax
+import torch.nn.functional as F
+from model.HolisticAttention import HA
+from torch.autograd import Variable
+from torch.distributions import Normal, Independent, kl
+import numpy as np
+
+class Generator(nn.Module):
+    def __init__(self, channel):
+        super(Generator, self).__init__()
+        self.sal_encoder = Saliency_feat_encoder(channel)
+
+
+    def forward(self, x):
+
+        sal_init_post, sal_ref_post = self.sal_encoder(x)
+        sal_init_post = F.upsample(sal_init_post, size=(x.shape[2], x.shape[3]), mode='bilinear',
+                                        align_corners=True)
+        sal_ref_post = F.upsample(sal_ref_post, size=(x.shape[2], x.shape[3]), mode='bilinear',
+                                       align_corners=True)
+        return sal_init_post, sal_ref_post
+
+
+class Classifier_Module(nn.Module):
+    def __init__(self,dilation_series,padding_series,NoLabels, input_channel):
+        super(Classifier_Module, self).__init__()
+        self.conv2d_list = nn.ModuleList()
+        for dilation,padding in zip(dilation_series,padding_series):
+            self.conv2d_list.append(nn.Conv2d(input_channel,NoLabels,kernel_size=3,stride=1, padding =padding, dilation = dilation,bias = True))
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.conv2d_list[0](x)
+        for i in range(len(self.conv2d_list)-1):
+            out += self.conv2d_list[i+1](x)
+        return out
+
+## Channel Attention (CA) Layer
+class CALayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(CALayer, self).__init__()
+        # global average pooling: feature --> point
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # feature channel downscale and upscale --> channel weight
+        self.conv_du = nn.Sequential(
+                nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=True),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+## Residual Channel Attention Block (RCAB)
+class RCAB(nn.Module):
+    # paper: Image Super-Resolution Using Very DeepResidual Channel Attention Networks
+    # input: B*C*H*W
+    # output: B*C*H*W
+    def __init__(
+        self, n_feat, kernel_size=3, reduction=16,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+
+        super(RCAB, self).__init__()
+        modules_body = []
+        for i in range(2):
+            modules_body.append(self.default_conv(n_feat, n_feat, kernel_size, bias=bias))
+            if bn: modules_body.append(nn.BatchNorm2d(n_feat))
+            if i == 0: modules_body.append(act)
+        modules_body.append(CALayer(n_feat, reduction))
+        self.body = nn.Sequential(*modules_body)
+        self.res_scale = res_scale
+
+    def default_conv(self, in_channels, out_channels, kernel_size, bias=True):
+        return nn.Conv2d(in_channels, out_channels, kernel_size,padding=(kernel_size // 2), bias=bias)
+
+    def forward(self, x):
+        res = self.body(x)
+        #res = self.body(x).mul(self.res_scale)
+        res += x
+        return res
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1):
+        super(BasicConv2d, self).__init__()
+        self.conv_bn = nn.Sequential(
+            nn.Conv2d(in_planes, out_planes,
+                      kernel_size=kernel_size, stride=stride,
+                      padding=padding, dilation=dilation, bias=False),
+            nn.BatchNorm2d(out_planes)
+        )
+
+    def forward(self, x):
+        x = self.conv_bn(x)
+        return x
+
+
+class Triple_Conv(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(Triple_Conv, self).__init__()
+        self.reduce = nn.Sequential(
+            BasicConv2d(in_channel, out_channel, 1),
+            BasicConv2d(out_channel, out_channel, 3, padding=1),
+            BasicConv2d(out_channel, out_channel, 3, padding=1)
+        )
+
+    def forward(self, x):
+        return self.reduce(x)
+
+class Edge_Module(nn.Module):
+
+    def __init__(self, in_fea=[256, 512, 1024], mid_fea=32):
+        super(Edge_Module, self).__init__()
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(in_fea[0], mid_fea, 1)
+        self.conv4 = nn.Conv2d(in_fea[1], mid_fea, 1)
+        self.conv5 = nn.Conv2d(in_fea[2], mid_fea, 1)
+        self.conv5_2 = nn.Conv2d(mid_fea, mid_fea, 3, padding=1)
+        self.conv5_4 = nn.Conv2d(mid_fea, mid_fea, 3, padding=1)
+        self.conv5_5 = nn.Conv2d(mid_fea, mid_fea, 3, padding=1)
+
+        self.classifer = nn.Conv2d(mid_fea * 3, 1, kernel_size=3, padding=1)
+        self.rcab = RCAB(mid_fea * 3)
+
+    def forward(self, x2, x4, x5):
+        _, _, h, w = x2.size()
+        edge2_fea = self.relu(self.conv2(x2))
+        edge2 = self.relu(self.conv5_2(edge2_fea))
+        edge4_fea = self.relu(self.conv4(x4))
+        edge4 = self.relu(self.conv5_4(edge4_fea))
+        edge5_fea = self.relu(self.conv5(x5))
+        edge5 = self.relu(self.conv5_5(edge5_fea))
+
+        edge4 = F.interpolate(edge4, size=(h, w), mode='bilinear', align_corners=True)
+        edge5 = F.interpolate(edge5, size=(h, w), mode='bilinear', align_corners=True)
+
+        edge = torch.cat([edge2, edge4, edge5], dim=1)
+        edge = self.rcab(edge)
+        edge = self.classifer(edge)
+        return edge
+
+class Saliency_feat_encoder(nn.Module):
+    # resnet based encoder decoder
+    def __init__(self, channel):
+        super(Saliency_feat_encoder, self).__init__()
+        self.resnet = B2_ResNet()
+        self.relu = nn.ReLU(inplace=True)
+        self.upsample8 = nn.Upsample(scale_factor=8, mode='bilinear', align_corners=True)
+        self.upsample4 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
+        self.upsample2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.upsample05 = nn.Upsample(scale_factor=0.5, mode='bilinear', align_corners=True)
+        self.dropout = nn.Dropout(0.3)
+        self.layer6 = self._make_pred_layer(Classifier_Module, [6, 12, 18, 24], [6, 12, 18, 24], 1, channel*4)
+        self.conv4 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 2048)
+        self.conv3 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 1024)
+        self.conv2 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 512)
+        self.conv1 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 256)
+
+        self.racb_43 = RCAB(channel * 2)
+        self.racb_432 = RCAB(channel * 3)
+        self.racb_4321 = RCAB(channel * 4)
+
+        self.conv43 = Triple_Conv(2*channel, channel)
+        self.conv432 = Triple_Conv(3 * channel, channel)
+        self.conv4321 = Triple_Conv(4 * channel, channel)
+
+        self.HA = HA()
+        self.conv4_2 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 2048)
+        self.conv3_2 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 1024)
+        self.conv2_2 = self._make_pred_layer(Classifier_Module, [3, 6, 12, 18], [3, 6, 12, 18], channel, 512)
+
+        self.racb_43_2 = RCAB(channel * 2)
+        self.racb_432_2 = RCAB(channel * 3)
+        self.racb_4321_2 = RCAB(channel * 4)
+
+        self.edge_layer = Edge_Module()
+
+        self.conv43_2 = Triple_Conv(2 * channel, channel)
+        self.conv432_2 = Triple_Conv(3 * channel, channel)
+        self.conv4321_2 = Triple_Conv(4 * channel, channel)
+        self.layer7 = self._make_pred_layer(Classifier_Module, [6, 12, 18, 24], [6, 12, 18, 24], 1, channel * 4)
+
+        if self.training:
+            self.initialize_weights()
+
+    def _make_pred_layer(self, block, dilation_series, padding_series, NoLabels, input_channel):
+        return block(dilation_series, padding_series, NoLabels, input_channel)
+
+    def tile(self, a, dim, n_tile):
+        """
+        This function is taken form PyTorch forum and mimics the behavior of tf.tile.
+        Source: https://discuss.pytorch.org/t/how-to-tile-a-tensor/13853/3
+        """
+        init_dim = a.size(dim)
+        repeat_idx = [1] * a.dim()
+        repeat_idx[dim] = n_tile
+        a = a.repeat(*(repeat_idx))
+        order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)])).to(device)
+        return torch.index_select(a, dim, order_index)
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+        x1 = self.resnet.layer1(x)  # 256 x 64 x 64
+        x2 = self.resnet.layer2(x1)  # 512 x 32 x 32
+        x3 = self.resnet.layer3_1(x2)  # 1024 x 16 x 16
+        x4 = self.resnet.layer4_1(x3)  # 2048 x 8 x 8
+
+        conv1_feat = self.conv1(x1)
+        conv2_feat = self.conv2(x2)
+        conv3_feat = self.conv3(x3)
+        conv4_feat = self.conv4(x4)
+        conv4_feat = self.upsample2(conv4_feat)
+
+        conv43 = torch.cat((conv4_feat, conv3_feat),1)
+        conv43 = self.racb_43(conv43)
+        conv43 = self.conv43(conv43)
+
+        conv43 = self.upsample2(conv43)
+        conv432 = torch.cat((self.upsample2(conv4_feat), conv43, conv2_feat), 1)
+        conv432 = self.racb_432(conv432)
+        conv432 = self.conv432(conv432)
+        conv432 = self.upsample2(conv432)
+        conv4321 = torch.cat((self.upsample4(conv4_feat), self.upsample2(conv43), conv432, conv1_feat), 1)
+        conv4321 = self.racb_4321(conv4321)
+
+        sal_init = self.layer6(conv4321)
+
+        x2_2 = self.HA(self.upsample05(sal_init).sigmoid(), x2)
+        x3_2 = self.resnet.layer3_2(x2_2)  # 1024 x 16 x 16
+        x4_2 = self.resnet.layer4_2(x3_2)  # 2048 x 8 x 8
+
+        conv2_feat = self.conv2_2(x2_2)
+        conv3_feat = self.conv3_2(x3_2)
+        conv4_feat = self.conv4_2(x4_2)
+        conv4_feat = self.upsample2(conv4_feat)
+
+        conv43 = torch.cat((conv4_feat, conv3_feat), 1)
+        conv43 = self.racb_43_2(conv43)
+        conv43 = self.conv43_2(conv43)
+
+        conv43 = self.upsample2(conv43)
+        conv432 = torch.cat((self.upsample2(conv4_feat), conv43, conv2_feat), 1)
+        conv432 = self.racb_432_2(conv432)
+
+        conv432 = self.conv432_2(conv432)
+
+        conv432 = self.upsample2(conv432)
+        conv4321 = torch.cat((self.upsample4(conv4_feat), self.upsample2(conv43), conv432, conv1_feat), 1)
+        conv4321 = self.racb_4321_2(conv4321)
+        sal_ref = self.layer7(conv4321)
+
+
+        return self.upsample8(sal_init), self.upsample4(sal_ref)
+
+    def initialize_weights(self):
+        res50 = models.resnet50(pretrained=True)
+        pretrained_dict = res50.state_dict()
+        all_params = {}
+        for k, v in self.resnet.state_dict().items():
+            if k in pretrained_dict.keys():
+                v = pretrained_dict[k]
+                all_params[k] = v
+            elif '_1' in k:
+                name = k.split('_1')[0] + k.split('_1')[1]
+                v = pretrained_dict[name]
+                all_params[k] = v
+            elif '_2' in k:
+                name = k.split('_2')[0] + k.split('_2')[1]
+                v = pretrained_dict[name]
+                all_params[k] = v
+        assert len(all_params.keys()) == len(self.resnet.state_dict().keys())
+        self.resnet.load_state_dict(all_params)
